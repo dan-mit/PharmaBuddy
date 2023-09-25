2343a106697470337535ee82f33c3608beaafeea
Daniil <daniilmitev@gmail.com>
2023-09-25 17:13:27 -0400

    Added a Nav Bar with some icons as well as a homepage

---
+++ lib/homepage.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+  }
+
+  class _HomePageState extends State<HomePage> {
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        bottomNavigationBar: GNav(
+          tabs: [
+            GButton(icon: Icons.home),
+            GButton(icon: Icons.search),
+            GButton(icon: Icons.schedule),
+            GButton(icon: Icons.pin_drop)
+          ],
+        ),
+      );
+    }
+  }
