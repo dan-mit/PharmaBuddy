9b8e9b95dfdb637a6c152035643fdd33938ad98b
Daniil <daniilmitev@gmail.com>
2023-12-04 19:47:57 -0500

    Added a pharmacyLocate model for the API as well as created the Locate Page

---
+++ lib/models/pharmacyLocate.dart
@@ -0,0 +1,43 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+Future<List<Place>> fetchPharmacies(double latitude, double longitude) async {
+  final apiKey = 'AIzaSyChywo2Fri_pQd_iuZzElRmkFGVwKc_C2c';
+  final response = await http.get(
+    Uri.parse(
+        'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=$latitude,$longitude&radius=5000&type=pharmacy&key=$apiKey'),
+  );
+
+  if (response.statusCode == 200) {
+    final data = json.decode(response.body);
+    final List<Place> pharmacies = [];
+    for (var place in data['results']) {
+      pharmacies.add(Place.fromJson(place));
+    }
+    return pharmacies;
+  } else {
+    throw Exception('Failed to load pharmacies');
+  }
+}
+
+class Place {
+  final String name;
+  final String address;
+  final double lat;
+  final double lng;
+
+  Place(
+      {required this.name,
+      required this.address,
+      required this.lat,
+      required this.lng});
+
+  factory Place.fromJson(Map<String, dynamic> json) {
+    return Place(
+      name: json['name'],
+      address: json['vicinity'],
+      lat: json['geometry']['location']['lat'],
+      lng: json['geometry']['location']['lng'],
+    );
+  }
+}
--- lib/pages/LocatePage.dart
+++ lib/pages/LocatePage.dart
@@ -1,11 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class LocatePage extends StatelessWidget {
+class LocatePage extends StatefulWidget {
+  @override
+  _LocatePageState createState() => _LocatePageState();
+}
+
+class _LocatePageState extends State<LocatePage> {
+  Location location = new Location();
+  bool _serviceEnabled;
+  PermissionStatus _permissionGranted;
+  LocationData _locationData;
+  GoogleMapController _mapController;
+  Set<Marker> _markers = {};
+
+  @override
+  void initState() {
+    super.initState();
+    initializeLocation();
+  }
+
+  void initializeLocation() async {
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    _locationData = await location.getLocation();
+    loadPharmacies(); // Load pharmacies and place markers on the map
+  }
+
+  void loadPharmacies() {
+    // Example: Load pharmacies and update state
+    // This should ideally be fetched from a database or API
+    setState(() {
+      _markers.add(
+        Marker(
+          markerId: MarkerId('pharmacy1'),
+          position: LatLng(_locationData.latitude, _locationData.longitude),
+          infoWindow: InfoWindow(
+              title: 'Pharmacy Name', snippet: 'Address, Phone Number'),
+        ),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('LocatePage'),
+        title: Text('Locate Pharmacies'),
+      ),
+      body: GoogleMap(
+        onMapCreated: (GoogleMapController controller) {
+          _mapController = controller;
+        },
+        initialCameraPosition: CameraPosition(
+          target: LatLng(_locationData.latitude, _locationData.longitude),
+          zoom: 14.0,
+        ),
+        markers: _markers,
       ),
     );
   }
